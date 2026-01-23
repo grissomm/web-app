3d7849fc12072b7e2352eba08ebd199aecc47424
eyen20 <24049601@myrp.edu.sg>
2026-01-23 23:08:00 +0800

    Forgot comma but hey, it prove the test works

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let dramas = [
   { id:2, title: 'Falling into Your Smile', episode:31, genre: 'Gaming', country: 'China', 
     image:'https://hivemindedness.com/wp-content/uploads/2022/09/falling-into-your-smile-promo.jpg'},
   { id:3, title: 'Weak Hero Class', episode:8, genre: 'School', country: 'Korean', 
-    image:'https://i.mydramalist.com/pq2lr_4f.jpg'}
+    image:'https://i.mydramalist.com/pq2lr_4f.jpg'},
   { id:4, title: 'Meteor Garden', episode:49, genre: 'Romance', country: 'Taiwan', 
     image:'https://upload.wikimedia.org/wikipedia/en/2/2a/Meteor_Garden_2018_Poster.jpg'}
 ];
