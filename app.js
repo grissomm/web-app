7d00c4dee7bb1fb857ccb1862ed32619d295a00a
eyen20 <24049601@myrp.edu.sg>
2026-01-27 09:36:48 +0800

    Change port

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(express.json());
 
